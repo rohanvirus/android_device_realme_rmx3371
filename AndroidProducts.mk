6d3e3d400f399f562475d0ea4c75caea3f771268
Amrutesh Naregal <107930548+DragonEmperor9480@users.noreply.github.com>
2023-06-04 19:54:39 +0530

    changes back to based on lineage tree

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/derp_spartan.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_spartan-user \
-    derp_spartan-userdebug \
-    derp_spartan-eng
+    lineage_spartan-user \
+    lineage_spartan-userdebug \
+    lineage_spartan-eng
